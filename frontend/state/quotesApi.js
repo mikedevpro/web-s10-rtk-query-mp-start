1979d27b5d117cfbda318d47f0eab06bc4a3126e
Michael Nobles <Mnobles33@gmail.com>
2025-01-01 13:57:30 -0700

    First commit

--- frontend/state/quotesApi.js
+++ frontend/state/quotesApi.js
@@ -1,1 +1,23 @@
 // create your RTK Query endpoints here
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+export const quotesApi = createApi({
+    reducerPath: 'quotesApi',
+    baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:9009/api/' }),
+    tagTypes: ['Quotes'],
+    endpoints: build => ({
+       getQuotes: build.query({
+         query: () => 'quotes',
+         providesQuotes: ['Quotes']  
+      }),
+      createQuote: build.mutation({
+
+      }),
+      toggleFake: build.mutation({
+
+      }),
+      deleteQuote: build.mutation({
+
+      }),
+   })
+})
